84ad3690843eddf6244577d03a70b69ffa8afa33
ChuaTeSimp <nhatdi2710@gmail.com>
2024-03-05 21:50:07 +0700

    pull code from github

--- app/config/index.js
+++ app/config/index.js
@@ -1,6 +1,9 @@
 const config = {
     app: {
         port: process.env.PORT || 3000,
+    },
+    db: {
+        uri: process.env.MONGODB_URI || "mongodb://127.0.0.1:271017/contactbook"
     }
 };
     
